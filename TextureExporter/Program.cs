0271db9173f0e42db1bf0a7a684e862ccf3335fa
Kang Il Son <opengl83@gmail.com>
2021-04-05 11:24:52 +0900

    update texture exporter

--- TextureExporter/Program.cs
+++ TextureExporter/Program.cs
@@ -14,31 +14,83 @@ namespace TextureExporter
 {
     public class Program
     {
-        static void Main(string[] args)
+        static bool IsPOT(int width, int height)
         {
+            int[] pots = { 4, 16, 32, 64, 128, 256, 512, 1024, 2048 };
+            foreach (var denominator in pots)
+            {
+                if (denominator <= width)
+                {
+                    if (width % denominator != 0)
+                    {
+                        return false;
+                    }
+                }
+
+                if(denominator <= height)
+                {
+                    if (height % denominator != 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        static bool IsCompressed(TextureFormat format)
+        {
+            var formatString= format.ToString();
+            if (formatString.StartsWith("ETC") ||
+                formatString.StartsWith("ASTC") ||
+                formatString.StartsWith("PVRTC") ||
+                formatString.StartsWith("DXT"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static int Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Usage : TextureExporter.exe androidAssetPath textureoutputpath textureSummaryPath");
+                return -1;
+            }
+
+            if (args.Length < 1 || !Directory.Exists(args[0].Trim()))
+            {
+                Console.WriteLine("{0} does not exit..", args[0]);
+                return -1;
+            }
+
+            if (!Directory.Exists(args[1].Trim()))
+            {
+                Directory.CreateDirectory(args[1]);
+            }
+
+            string exportDir = args[1].Trim();
+
             var assetManager = new AssetsManager();
             try
             {
-                assetManager.LoadFolder(@"C:\Users\ssogal\Desktop\Android");
+                assetManager.LoadFolder(args[0]);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
 
-            var containerDic = new Dictionary<AssetStudio.Object, string>();
-            var objectCount = assetManager.assetsFileList.Sum(x => x.Objects.Count);
-            var objectAssetItemDic = new Dictionary<Object, AssetItem>(objectCount);
             var objectAssetPaths = new Dictionary<Object, HashSet<string>>();
             var containers = new List<(PPtr<Object>, string)>();
 
             foreach (var asset in assetManager.assetsFileList)
             {
-                List<JObject> textures = new List<JObject>();
-                
                 foreach (var obj in asset.Objects)
                 {
-                    objectAssetItemDic.Add(obj, new AssetItem(obj));
                     objectAssetPaths.Add(obj, new HashSet<string>());
 
                     switch (obj)
@@ -71,99 +123,102 @@ namespace TextureExporter
             {
                 if (pptr.TryGet(out var obj))
                 {
-                    objectAssetItemDic[obj].Container = container;
                     objectAssetPaths[obj].Add(container);
                 }
             }
 
-            Dictionary<string, JArray> atlasMap = new Dictionary<string, JArray>();
 
+            List<JObject> textures = new List<JObject>();
+            List<JObject> npotTextures = new List<JObject>();
+            List<JObject> uncompressedTextures = new List<JObject>();
+            
             foreach (var asset in assetManager.assetsFileList)
             {
-                foreach(var obj in asset.Objects)
-                {
-                    switch (obj)
-                    {
-                        case SpriteAtlas atlas:
-                            if (!atlasMap.ContainsKey(atlas.m_Name))
-                            {
-                                atlasMap.Add(atlas.m_Name, new JArray());
-
-                                foreach (var sprite in atlas.m_PackedSprites)
-                                {
-                                    if (sprite.TryGet(out var result))
-                                    {
-                                        if (result.m_RD.texture.TryGet(out var texture))
-                                        {
-                                            atlasMap[atlas.m_Name].Add(texture.m_Name);
-                                        }
-                                    }
-                                }
-                            }
-                            break;
-                    }
-                }
-            }
-
-            foreach (var asset in assetManager.assetsFileList)
-            {
-                List<JObject> textures = new List<JObject>();
-
-                
-
                 foreach (var obj in asset.Objects)
                 {
                     switch (obj)
                     {
                         case Texture2D texture:
                             List<string> paths = objectAssetPaths[obj].ToList();
-                            
+                            var possiblePath = paths.Where(x => x.Contains($"{texture.m_Name}.jpg") ||
+                                                                x.Contains($"{texture.m_Name}.tga") ||
+                                                                x.Contains($"{texture.m_Name}.png") ||
+                                                                x.Contains($"{texture.m_Name}.psd") ||
+                                                                x.Contains($"{texture.m_Name}.bmp")).FirstOrDefault();
+
+                            if (possiblePath == null || possiblePath == string.Empty)
+                            {
+                                possiblePath = paths.Where(x => x.Contains($"{texture.m_Name}")).FirstOrDefault();
+                                if (possiblePath == null)
+                                {
+                                    possiblePath = $"MaybeAtlas/{texture.m_Name}";
+                                }
+                            }
+
                             ClassIDType id = (ClassIDType) obj.serializedType.classID;
                             var jobject = new JObject(
                                 new JProperty("name", texture.m_Name),
                                 new JProperty("path", paths),
+                                new JProperty("possiblePath", possiblePath),
                                 new JProperty("classId", id.ToString()),
-                                new JProperty("width", texture.m_Width),
-                                new JProperty("height", texture.m_Height),
+                                new JProperty("size", $"{texture.m_Width} x {texture.m_Height}"),
                                 new JProperty("format", texture.m_TextureFormat.ToString()),
                                 new JProperty("mipsCount", texture.m_MipCount),
-                                new JProperty("mipmap", texture.m_MipMap),
-                                new JProperty("streamPath", (texture.m_StreamData != null) ? texture.m_StreamData.path : ""),
-                                new JProperty("size", (texture.m_StreamData != null) 
-                                    ? obj.byteSize +  texture.m_StreamData.size : obj.byteSize));
+                                new JProperty("mipmap", texture.m_MipMap ? "true" : "false"),
+                                new JProperty("npot", IsPOT(texture.m_Width, texture.m_Height) ? "false" : "true"),
+                                new JProperty("byteSize", (texture.m_StreamData != null)
+                                    ? obj.byteSize + texture.m_StreamData.size
+                                    : obj.byteSize));
+
+                            if (!IsPOT(texture.m_Width, texture.m_Height))
+                            {
+                                npotTextures.Add(jobject);
+                            }
+
+                            if (!IsCompressed(texture.m_TextureFormat))
+                            {
+                                uncompressedTextures.Add(jobject);
+                            }
+
                             textures.Add(jobject);
-                            break;
-
-                        
-                        case Sprite sprite:
-                            
+                            Exporter.ExportTexture2D(texture, Path.Combine(exportDir, Path.GetDirectoryName(possiblePath)));
+                            Console.WriteLine("Exporting {0}", Path.Combine(exportDir,possiblePath));
                             break;
                     }
-                }
-                
-                var assetObject = new JObject(
-                    new JProperty("assetFileName", asset.fileName),
-                    new JProperty("textures", new JArray(textures))
-                );
-
-                try
-                {
-                    using (var file = File.CreateText($"{asset.fileName}.json"))
-                    using (var writer = new JsonTextWriter(file)
-                    {
-                        Indentation = 2,
-                        Formatting = Formatting.Indented,
-                        AutoCompleteOnClose = true
-                    })
-                    {
-                        assetObject.WriteTo(writer);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
                 }
             }
+
+            textures = textures.OrderByDescending(x => x["byteSize"]).ToList();
+            uncompressedTextures = uncompressedTextures.OrderByDescending(x => x["byteSize"]).ToList();
+            npotTextures = npotTextures.OrderByDescending(x => x["byteSize"]).ToList();
+
+            var assetObject = new JObject(
+                new JProperty("textures", new JArray(textures)),
+                new JProperty("uncompressed" , new JArray(uncompressedTextures)),
+                new JProperty("npot" , new JArray(npotTextures))
+            );
+
+            try
+            {
+                using (var file = File.CreateText(args[2].Trim()))
+                using (var writer = new JsonTextWriter(file)
+                {
+                    Indentation = 2,
+                    Formatting = Formatting.Indented,
+                    AutoCompleteOnClose = true
+                })
+                {
+                    assetObject.WriteTo(writer);
+                    Console.WriteLine("Success");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+
+            return 0;
         }
     }
 }
